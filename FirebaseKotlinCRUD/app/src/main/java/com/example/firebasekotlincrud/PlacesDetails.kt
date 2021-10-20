8b2c37455b73b697ae785ff4d59721c296349636
Ernesto2200 <ernesto21rosales@gmail.com>
2021-10-20 11:17:05 -0600

    Implementación_Favoritos

--- FirebaseKotlinCRUD/app/src/main/java/com/example/firebasekotlincrud/PlacesDetails.kt
+++ FirebaseKotlinCRUD/app/src/main/java/com/example/firebasekotlincrud/PlacesDetails.kt
@@ -3,9 +3,11 @@ package com.example.firebasekotlincrud
 import android.os.Bundle
 import android.text.Editable
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.example.firebasekotlincrud.databinding.ActivityPlacesDetailsBinding
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
@@ -32,12 +34,15 @@ class PlacesDetails : AppCompatActivity() {
 
     val favorite = true
     myRef.child("favorite").setValue(favorite)
+            Toast.makeText(applicationContext, "Añadido a favoritos", Toast.LENGTH_SHORT).show()
     finish()
 
         }
 binding.FavoriteActionButton.setOnLongClickListener {
+
     val favorite = false
     myRef.child("favorite").setValue(favorite)
+    Toast.makeText(applicationContext, "Eliminado de favoritos", Toast.LENGTH_SHORT).show()
     finish()
     true
 }
